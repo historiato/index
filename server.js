5f217a86eecd8fe07dc54d1eaaba853ae491d23a
historiato <historiato@yahoo.com>
2024-12-03 07:45:12 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+// Configuración del servidor
+const hostname = '0.0.0.0'; // Dirección IP (localhost)
+const port = 3000; // Puerto donde escuchará el servidor
+
+// Creación del servidor
+const server = http.createServer((req, res) => {
+  // Configuración de la respuesta
+  res.statusCode = 200; // Código HTTP de éxito
+  res.setHeader('Content-Type', 'text/plain'); // Tipo de contenido
+  res.end('¡Hola, mundo!'); // Respuesta del servidor
+});
+
+// Inicio del servidor
+server.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
